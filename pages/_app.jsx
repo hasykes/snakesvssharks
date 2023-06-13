4d7587f2b6c6aa5a407fb9f7a9899fcda75fd67f
Hayden Sykes <haydensykes13@gmail.com>
2023-06-13 17:44:37 -0400

    force site to dark theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,7 @@ import { Toaster } from 'react-hot-toast';
 export default function MyApp({ Component, pageProps }) {
 
   return (
-    <ThemeProvider>
+    <ThemeProvider forcedTheme = 'dark'>
       <Layout>
         <Component {...pageProps} />
         <Toaster />
