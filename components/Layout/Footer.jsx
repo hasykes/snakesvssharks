10f2dc8c80b88435017f5a64bd3fcbd32e5b005c
Hayden Sykes <haydensykes13@gmail.com>
2023-06-15 19:27:52 -0400

    add footer and copyright

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -4,14 +4,16 @@ import Spacer from './Spacer';
 import Wrapper from './Wrapper';
 
 const Footer = () => {
+
+  var d = new Date(); 
+  const currentYear = d.getFullYear();
+
   return (
     <footer className={styles.footer}>
       <Wrapper>
         <Text color="accents-7">
-        
+        <small>&copy; Copyright {currentYear}, Hayden Sykes</small>
         </Text>
-        <Spacer size={1} axis="vertical" />
-
       </Wrapper>
     </footer>
   );
